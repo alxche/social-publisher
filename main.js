5f550c27c587f9b875affabfa1337251c8f67426
alexche <clawfinga@gmail.com>
2015-01-28 06:11:35 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,50 @@
+socialPublisher=function(options){
+    this.options = options;
+}
+
+
+socialPublisher.prototype = {
+    OAuth: Package.oauth1.OAuth1Binding,
+    post: function(text){
+        for(var i in this.options) {
+            this.proceed(i,this.options[i],text);
+        }
+    },
+    proceed: function(service, config, text){
+        if(service=="twitter"){
+
+            var oa = new this.OAuth({
+                consumerKey: config.consumerKey,
+                secret: config.secret
+            });
+            oa.accessToken = config.accessToken;
+            oa.accessTokenSecret = config.accessTokenSecret;
+            var tweet = {
+                status: text,
+                in_reply_to_status_id: null
+            }
+
+            var result = oa.call('POST',
+                "https://api.twitter.com/1.1/statuses/update.json",
+                tweet
+            );
+        }
+
+        if(service=="facebook"){
+
+            var oa = new this.OAuth({
+                consumerKey: config.consumerKey,
+                secret: config.secret
+            });
+            oa.accessToken = config.accessToken;
+            var msg = {
+                message: text
+            }
+
+            var result = oa.call('POST',
+                "https://graph.facebook.com/v2.2/1783198641/feed?access_token="+config.accessToken,
+                msg
+            );
+        }
+    }
+}
---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'alexche:social-publisher',
+  summary: 'facebook and twitter status update library ',
+  version: '1.0.0',
+  git: 'https://github.com/alxche/social-publisher.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.2.1');
+  api.addFiles('main.js');
+  api.export('socialPublisher');
+});
+
+
